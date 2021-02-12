f77e9e45e61f8b8762e03c42d2493db954b92c37
Alexandre Lourencinho <alexandre.lourencinho@gmail.com>
2021-02-12 12:17:08 +0100

    fin de semaine push

--- js/exo 9-2/js.js
+++ js/exo 9-2/js.js
@@ -1,27 +1,44 @@
-var str1;
+
 var str2 = prompt("entrez le séparateur voulu");
 var n;
 var tablo = [];
-function strtok()
-{
-        while ((str1 = prompt("entre un mot")))
+var str1;
+function strtok() {
+
+        while ((str1 = prompt("entre un mot"))) 
         {
             var cha = cha + str2 + str1;
         }
-    console.log(cha);
-        if (cha!="" && str2!="")
+        console.log(cha);
+        if (cha != "" && str2 != "") 
         {
             tablo = cha.split(str2);
         }
-    console.log(tablo);
-        if (tablo.length > 0) {
+        console.log(tablo);
+        str1="nbg";
+        if (tablo.length > 0) 
+        {
             n = prompt("quel mot souhaitez vous retrouver?");
             n = parseInt(n);
+            if (n < tablo.length && n > 0) 
+            {
             alert(tablo[n]);
+            } 
+            else 
+            {
+            alert("enfoiré de petit malin, tu m'a eu!");
+            }
         } 
         else 
         {
             alert("vous n'avez rien indiqué!");
         }
 }
-strtok(str1,str2,n)
+strtok();
+// var n = 2 ;
+// strtok()
+// console.log(str1)
+// console.log(str2)
+console.log(n)
+ if (n>0)
+ { alert("blablabla");}
