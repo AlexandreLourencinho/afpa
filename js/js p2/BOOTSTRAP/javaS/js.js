f8ecad64355a4fa60246c44fa76ada38d35374d3
Alexandre Lourencinho <alexandre.lourencinho@gmail.com>
2021-02-16 15:28:04 +0100

    fin eval presentation a faire

--- js/js p2/BOOTSTRAP/javaS/js.js
+++ js/js p2/BOOTSTRAP/javaS/js.js
@@ -1,133 +1,126 @@
-var fnom= new RegExp(/^[A-Za-z]+$/);
-var fdate = new RegExp(/^[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{2,4}$/);
-var fmail= new RegExp(/^\S+@\S+\.\S+$/);
+
+
+// expressions régulières -----------------------------------------------------------------------------------
+var fnom= new RegExp(/^[A-Za-zéàï -]+$/);
+// var fdate = new RegExp(/^[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{2,4}$/);
+// var fmail= new RegExp(/^\S+@\S+\.\S+$/);
 var fcodep = new RegExp(/^[0-9]{5}$/);
 var fville = new RegExp(/^[a-zA-z -]+$/);
 var fadresse = new RegExp (/^[A-Za-z0-9 -]+$/);
-// var nomt=document.getElementById("nom").value
-// var datet = document.getElementById("date").value
-// var mailt = document.getElementById("mail").value
-// var codept= document.getElementById("codep").value
-// var villet = document.getElementById("ville").value
-// var prenomt = document.getElementById(" prenom").value
-// var adresset = document.getElementById("adresse").value
-// var nomt=fnom.test("nom")
-// console.log(nomt)
-// function checkRadio() {
-//     var r = document.getElementsByName("sexe")
-//     var c = -1
-    
-//     for(var i=0; i < r.length; i++){
-//        if(r[i].checked) {
-//           c = i; 
-//        }
-//     }
-//     if (c == -1) alert("please select radio");
-//     }
+// fonction checkform-----------------------------------------------------------------------------------
 function checkForm(form)
 {
-    //   check nom vide
+    //   check nom vide-----------------------------------------------------------------------------------
     if(form.nom.value == "") {
         document.getElementById("nomerr").style.color = "#ff0000";
         document.getElementById("nomerr").innerHTML = "Le champ est vide!";
         form.nom.focus();
         return false;
     }  
-// chek nom regex
+// chek nom regex-----------------------------------------------------------------------------------
     if(!fnom.test(form.nom.value)) {
         document.getElementById("nomerr").style.color = "#ff0000";
         document.getElementById("nomerr").innerHTML = "Uniquement des lettres majuscules ou minuscules";
         form.nom.focus();
         return false;
     }
-    //   check  prenom vide
+    //   check  prenom vide-----------------------------------------------------------------------------------
     if(form.prenom.value == "") {
-        document.getElementById("prenomerr").style.color = "#ff0000";
-        document.getElementById("prenomerr").innerHTML = "Le champ est vide!";
+        document.getElementById("nomerr").innerHTML="";
+        document.getElementById("prenerr").style.color = "#ff0000";
+        document.getElementById("prenerr").innerHTML = "Le champ est vide!";
         form.prenom.focus();
         return false;
     }
-//     // check  prenom regex 
+//     // check  prenom regex -----------------------------------------------------------------------------------
     if(!fnom.test(form.prenom.value)) {
-        document.getElementById("prenomerr").style.color = "#ff0000";
-        document.getElementById("prenomerr").innerHTML = "Uniquement des lettres majuscules ou minuscules";
+        document.getElementById("prenerr").style.color = "#ff0000";
+        document.getElementById("prenerr").innerHTML = "Uniquement des lettres majuscules ou minuscules";
         form.prenom.focus();
         return false;
     }        
-        //   check mail vide
-    if(form.mail.value == "") {
-        document.getElementById("emailerr").style.color = "#ff0000";
-        document.getElementById("emailerrl").innerHTML = "Le champ est vide!";
-            form.mail.focus();
-            return false;
-    }  
-        //   check mail regex
-    if(!fmail.test(form.mail.value)) {
-            mail.setCustomValidity("le format d'email n'est pas valide. utilisez un email type damien.rieu@nomdomaine.fr");
-            form.mail.focus();
-            return false;
-    }
-        //   check code postal vide
+    // check code postal vide ---------------------------------------------------------------------------------------
     if(form.codep.value == "") {
-            codep.setCustomValidity("Le champ est vide!");
+        document.getElementById("nomerr").innerHTML="";
+        document.getElementById("prenerr").innerHTML ="";
+        document.getElementById("codeperr").style.color = "#ff0000";
+        document.getElementById("codeperr").innerHTML ="le champs est vide!";
             form.codep.focus();
             return false;
     }
-    // check code postal regex
+    // check code postal regex-----------------------------------------------------------------------------------
     if(!fcodep.test(form.codep.value)) {
-        codep.setCustomValidity("le code postal n'est composé que de 5 chiffres.");
-        form.mail.focus();
+        document.getElementById("nomerr").innerHTML="";
+        document.getElementById("prenerr").innerHTML ="";
+        document.getElementById("codeperr").style.color = "#ff0000";
+        document.getElementById("codeperr").innerHTML ="entrez un code postal au format valide. exemple : 84560"
+        form.codep.focus();
         return false;
     }
-// check ville vide
+// check ville vide-----------------------------------------------------------------------------------
     if(form.ville.value == "") {
-        ville.setCustomValidity("Le champ est vide!");
+        document.getElementById("codeperr").innerHTML="";
+        document.getElementById("nomerr").innerHTML="";
+        document.getElementById("prenerr").innerHTML ="";
+        document.getElementById("villerr").style.color = "#ff0000";
+        document.getElementById("villerr").innerHTML ="le champs est vide!"
         form.ville.focus();
         return false;
 }
-// check ville regex
+// check ville regex-----------------------------------------------------------------------------------
     if(!fville.test(form.ville.value)) {
-        ville.setCustomValidity("Entrez un nom de ville!");
+        document.getElementById("codeperr").innerHTML="";
+        document.getElementById("nomerr").innerHTML="";
+        document.getElementById("prenerr").innerHTML ="";
+        document.getElementById("villerr").style.color = "#ff0000";
+        document.getElementById("villerr").innerHTML ="Entrez un nom de ville!"
         form.ville.focus();
         return false;
     }
-  // check adresse vide
+  // check adresse vide-----------------------------------------------------------------------------------
     if(form.adresse.value == "") {
-    adresse.setCustomValidity("Le champ est vide!");
+        document.getElementById("villerr").innerHTML="";
+        document.getElementById("codeperr").innerHTML="";
+        document.getElementById("nomerr").innerHTML="";
+        document.getElementById("prenerr").innerHTML ="";
+        document.getElementById("adresserr").style.color = "#ff0000";
+        document.getElementById("adresserr").innerHTML ="Le champ est vide!";
     form.adresse.focus();
     return false;
     }
-// check adresse regex
+// check adresse regex-----------------------------------------------------------------------------------
     if(!fadresse.test(form.adresse.value)) {
-    adresse.setCustomValidity("Entrez une adresse valide!");
+        document.getElementById("villerr").innerHTML="";
+        document.getElementById("codeperr").innerHTML="";
+        document.getElementById("nomerr").innerHTML="";
+        document.getElementById("prenerr").innerHTML ="";
+        document.getElementById("adresserr").style.color = "#ff0000";
+        document.getElementById("adresserr").innerHTML = "Entrez une adresse valide"
     form.adresse.focus();
     return false;
     }  
-    // check radio value
-    if(radioValue = checkRadio(form.sexe)) {
-        ("Error: No value was selected!");
-        return false;
-      }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    // zone de texte----------------------------------------------------------------------------------------
+    if(form.reclamation.value == "") {
+        document.getElementById("villerr").innerHTML="";
+        document.getElementById("codeperr").innerHTML="";
+        document.getElementById("nomerr").innerHTML="";
+        document.getElementById("prenerr").innerHTML ="";
+        document.getElementById("adresserr").innerHTML="";
+        document.getElementById("texterr").style.color = "#ff0000";
+        document.getElementById("texterr").innerHTML ="Le champ est vide!";
+    form.reclamation.focus();
+    return false;
+    }
+    // checkbox -----------------------------------------------------------------------------------
     if(form.Check1.checked==false) {
-        Check1.setCustomValidity("Vous devez cocher cette case pour nous autoriser à traiter ce formulaire.");
+        document.getElementById("villerr").innerHTML="";
+        document.getElementById("codeperr").innerHTML="";
+        document.getElementById("nomerr").innerHTML="";
+        document.getElementById("prenerr").innerHTML ="";
+        document.getElementById("adresserr").innerHTML="";
+        document.getElementById("texterr").innerHTML="";
+        document.getElementById("checkerr").style.color = "#ff0000";
+        document.getElementById("checkerr").innerHTML = "Vous devez cocher cette case pour nous autoriser a traiter le formulaire.";
         return false;
       }
     //   validation was successful
@@ -135,19 +128,3 @@ function checkForm(form)
 
 
 }
-
-// function checkName(nom){
-//         //   check nom vide
-//     if(form.nom.value == "") {
-//         nom.setCustomValidity("Le champ est vide!");
-//         form.nom.focus();
-//         return false;
-//     }  
-// // chek nom regex
-//     if(!fnom.test(form.nom.value)) {
-//         nom.setCustomValidity("Uniquement des lettres majuscules ou minuscules");
-//         form.nom.focus();
-//         return false;
-//     }
-
-// }
---
+++ js/js p2/exos eval/exo 3/js.js
@@ -0,0 +1,17 @@
+var tab = ["Audrey", "Aurélien", "Flavien", "Jérémy", "Laurent", "Melik", "Nouara", "Salem", "Samuel", "Stéphane"];
+while(window.confirm("voulez-vous tenter de trouver un prénom?")==true)
+{
+    var prenom=prompt("entrez un prénom :")
+    if (tab.includes(prenom, [0]))
+    {
+        alert("vous avez trouvé "+prenom+" !")
+        var index=tab.indexOf(prenom);
+        tab.splice(index, 1);
+        tab.push(" ");
+        console.log(tab);
+
+    }
+    else{
+        alert("le prénom n'est pas dans le tableau!")
+    }
+}
