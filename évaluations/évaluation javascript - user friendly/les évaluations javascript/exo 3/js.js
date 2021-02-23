b5ae891cca0248aa1f7a83daca55ee2f5a6c7c5c
Alexandre Lourencinho <alexandre.lourencinho@gmail.com>
2021-02-23 11:04:14 +0100

    modification affichage exo tableau eval js

--- évaluations/évaluation javascript - user friendly/les évaluations javascript/exo 3/js.js
+++ évaluations/évaluation javascript - user friendly/les évaluations javascript/exo 3/js.js
@@ -6,34 +6,40 @@
 //  var tab = ["Audrey", "Aurélien", "Flavien", "Jérémy", "Laurent", "Melik", "Nouara", "Salem", "Samuel", "Stéphane"];
 // ( exemple : ["Audrey", "Aurélien", "Flavien", "Jérémy", "Laurent", "Melik", "Nouara", "Salem", "Samuel", " "]; )
 
-
+var tab = ["Audrey", "Aurélien", "Flavien", "Jérémy", "Laurent", "Melik", "Nouara", "Salem", "Samuel", "Stéphane"];
 // LE TABLEAU DES PRENOMS
 function tableau(){
-    // L'AFFICHAGE DU TABLEAU DES PRENOMS EST DANS LE HTML. SINONT : DANS UNE ALERT AU BESOIN
-var tab = ["Audrey", "Aurélien", "Flavien", "Jérémy", "Laurent", "Melik", "Nouara", "Salem", "Samuel", "Stéphane"];
-// alert (tab) 
-while (window.confirm("Voulez-vous trouver un prénom de la liste? appuyez sur annuler pour arrêter.")==true){
-// LA BOUCLE POUR TROUVER UN PRENOM
-    // LE PROMPT DEMANDE LE PRENOM
-    var prenom=prompt("entrez un prénom de la liste :")
     // SI LE PRENOM EST DANS LE TABLEAU, RECUPERE SON INDEX, ET LE VIRE POUR LE REMPLACER PAR UN " " 
+    var prenom=document.getElementById("prenomsup").value
     if (tab.includes(prenom, [0]))
     {
-        alert("vous avez rentré "+prenom+" !")
+        document.getElementById("prenomr").innerHTML="vous avez rentré "+prenom+" !";
         var index=tab.indexOf(prenom);
         tab.splice(index, 1);
         tab.push(" ");
         console.log(tab);
-
+        // affichage prenoms restants + le prenom rentré + cadre en vert + vide messsage d'erreur
+        document.getElementById("affi").innerHTML = "Voici les prénoms restants de la liste :" +"<br>"+tab+"<br>";
+        document.getElementById('prenomsup').style.borderColor = "limegreen";
+        document.getElementById("erreur").innerHTML = "";
     }
     // SI LE PRENOM N'EST PAS DANS LE TABLEAU
     else{
-        alert("Ce n'est pas dans la liste !")
+        // vide message succès + cadre en rouge + informe que prenom pas dans la liste
+        document.getElementById("erreur").innerHTML = "Le prénom n'est pas dans la liste!";
+        document.getElementById('prenomsup').style.borderColor = "red";
+        document.getElementById("prenomr").innerHTML="";
     }
-    // afiche ce qu'il reste de prénoms de la liste initiale.
-    document.getElementById("affi").innerHTML = "Voici les prénoms restants de la liste :" +"<br>"+tab+"<br>";
 }
+function reload()
+{
+    document.location.reload()
 }
+
+
+
+
+// }
 // NOTE : l'affichage s'actualise au fur et a mesure des prénoms
 // rentrés sans faire "annuler" sur firefox, mais pas sur Opéra  et sur Chrome. Probablement
 // une subtilité de ces navigateurs à prendre en compte sur le script dont je n'ai pas connaissance.
